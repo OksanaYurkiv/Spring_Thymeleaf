e722f2ffd2c0b05ef1ecc0e0d4a78ce9b1108fb8
OksanaYurkiv <oksana.yurkiv.1991@gmail.com>
2021-03-25 13:32:36 +0100

    Agregamos un campo de saldo a la base de datos y respectivamente a clase Persona

--- src/main/java/com/oksana/domain/Persona.java
+++ src/main/java/com/oksana/domain/Persona.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -27,13 +28,17 @@ public class Persona implements Serializable {
 	@NotEmpty
 	private	String nombre;
 	
-	@NotEmpty
+	@NotEmpty // NotEmpty para cadenas
 	private	String apellido;
+	
 	@NotEmpty
 	@Email
 	private	String email;
 	
 	private	String telefono;
+	
+	@NotNull
+	private Double saldo;
 
 
 }
